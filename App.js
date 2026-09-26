950427b941634213553275f3b588e87e26f2e032
Little4Letters <sjackson.andante@gmail.com>
2026-09-26 18:30:59 +0800

    Proj. Progress 1: Blank home and add section screen and implement bottom tab navigation, when tapped the 2 bottom navigation will display Home Screen and Add Habit Screen

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+//imported NavigationContainer and createBottomTabNavigator from react-navigation
+// to create a bottom tab navigator for the app.
+
+// (An gin build ini sa package.json file sa dependencies nga may version nga 6.1.6...
+// para sa @react-navigation/native ug 6.5.7 para sa @react-navigation/bottom-tabs)
+
+// The app has two screens: HomeScreen and AddHabitScrn, which are displayed as tabs in the navigator. The header is hidden for both screens using the screenOptions prop.
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+import HomeScreen from "./src/screens/HomeScreen.js";
+import AddHabitScrn from "./src/screens/AddHabitScrn.js";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Add" component={AddHabitScrn} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
